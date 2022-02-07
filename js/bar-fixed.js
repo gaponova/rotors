5ca0d05c9240a365f29e901c73a93c3580d423d2
gaponova <dzensmart@gmail.com>
2022-02-07 20:41:57 +0200

    Пофиксили отступы bar-fixed

--- js/bar-fixed.js
+++ js/bar-fixed.js
@@ -3,9 +3,9 @@ window.onscroll = () => {
   const headerbar = document.querySelector('.header');
   const Y = window.scrollY;
 
-  if (Y > 300) {
+  if (Y > 100) {
     headerbar.classList.add('bar-fixed');
-  } else if (Y < 300) {
+  } else if (Y < 100) {
     headerbar.classList.remove('bar-fixed');
   }
 };
