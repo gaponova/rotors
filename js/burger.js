42ec4c1e2aa1a288f2c7f4275c8158f9714ac4bc
gaponova <dzensmart@gmail.com>
2022-02-09 17:27:37 +0200

    Сделали секцию hero

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,7 @@
+/*Появление адаптивного меню*/
 $(document).ready(function () {
   $('.header__burger').click(function (event) {
     $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
   });
 });
